4c06b66eae697f261a10417b528f25bad363a9fb
George Gruenhagen <ggruenhagen3@gatech.edu>
2023-06-16 15:54:04 -0400

    Comparison of cichlid and turtle cells/nuclei

---
+++ cross_species/samap/cichlid_sc_turtle.py
@@ -0,0 +1,43 @@
+# conda activate SAMap
+# Imports
+from samap.mapping import SAMAP
+from samap.analysis import (get_mapping_scores, GenePairFinder,
+                            sankey_plot, chord_plot, CellTypeTriangles, 
+                            ParalogSubstitutions, FunctionalEnrichment,
+                            convert_eggnog_to_homologs, GeneTriangles)
+from samalg import SAM
+import pandas as pd
+import numpy as np
+
+# BLAST Results
+map_dir = '/storage/home/hcoda1/6/ggruenhagen3/p-js585-0/George/rich_project_pb1/bin/samap_directory/turtle_mz/maps/'
+mm_prot = pd.read_csv('/storage/home/hcoda1/6/ggruenhagen3/scratch/m_zebra_ref/turtle_prot_table3.csv', sep=None)
+mz_prot = pd.read_csv('/storage/home/hcoda1/6/ggruenhagen3/scratch/m_zebra_ref/mz_prot_table3.csv', sep=None)
+mm_prot_tup = list(zip(mm_prot["protein_id"], mm_prot["gene"]))
+mz_prot_tup = list(zip(mz_prot["protein_id"], mz_prot["gene"]))
+protein_to_gene_names = {'mz': mz_prot_tup, 'cp':mm_prot_tup}
+
+# Objects containing gene x counts
+fn1 = '/storage/home/hcoda1/6/ggruenhagen3/scratch/brain/data/bb.h5ad'
+fn2 = '/storage/coda1/p-js585/0/ggruenhagen3/George/rich_project_pb1/data/bcs/data/turtle.h5ad'
+filenames = {'mz':fn1,'cp':fn2}
+sam1=SAM()
+sam1.load_data(fn1)
+sam2=SAM()
+sam2.load_data(fn2)
+sams = {'mz':fn1,'cp':fn2}
+sm = SAMAP(sams, names=protein_to_gene_names, f_maps = map_dir)
+sm.run()
+samap = sm.samap
+
+# Save samap
+import pickle
+with open('/storage/home/hcoda1/6/ggruenhagen3/scratch/bcs/data/bb_turtle_samap.pkl', 'wb') as out_file:
+    pickle.dump(samap, out_file)
+
+
+with open('/storage/home/hcoda1/6/ggruenhagen3/scratch/bcs/data/bb_turtle_sm_ran.pkl', 'wb') as out_file:
+    pickle.dump(sm, out_file)
+
+# Export as h5ad so it can be loaded into Seurat
+samap.adata.write_h5ad("/storage/home/hcoda1/6/ggruenhagen3/scratch/bcs/data/bb_turtle.h5ad")
